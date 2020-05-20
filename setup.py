0f50397eddca2047eba50d4a815a72ec2425cf5a
mberneti <mberneti@live.com>
2020-05-20 10:22:30 +0430

    init

---
+++ setup.py
@@ -0,0 +1,6 @@
+import nltk
+
+# https://www.nltk.org/data.html
+# Once you’ve installed NLTK with pip install nltk,
+# you should install the NLTK packages by running the following code
+nltk.download()
